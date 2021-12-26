c45d6c5cd111fc6f0622cab9c765a66155c5a70d
Lucas Felici <lucas.a.felici@gmail.com>
2021-12-26 02:35:53 -0300

    first db intent

--- src/dao/JDBCaoImp.java
+++ src/dao/JDBCaoImp.java
@@ -4,19 +4,30 @@ import entities.Products;
 import helpers.JDBCHelper;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCaoImp {
 
-    public Products products() throws SQLException {
+    public Products getProducts(){
         Products products = new Products();
 
-        try {
-            Connection conn = JDBCHelper.getConnection();
-            String SQLSELECTALL = "SELECT * FROM products"
-        }catch (Exception e ){
+        try (Connection conn = JDBCHelper.getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM products")) {
+
+            while (rs.next()){
+                System.out.println("ID: " + rs.getInt("product_ID"));
+                System.out.println("Nombre: " + rs.getString("product_name"));
+                products.setProduct_ID(rs.getInt("product_ID"));
+                products.setProduct_name(rs.getString("product_name"));
+            }
+
+
+        }catch (SQLException e ){
             e.printStackTrace();
         }
+        return products;
     }
 }
